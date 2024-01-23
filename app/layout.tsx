3c61a7892e547366ce015403210f63cc4f295a71
Muhammad Angga Adytya <anggaanggaksp@gmail.com>
2024-01-23 13:46:57 +0700

    feat: add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,42 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL(
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:3000"
+      : process.env.DOMAIN || ""
+  ),
   title: "Weather App",
   description: "Generated Weather App by AnggaAdytya",
+  keywords: "weather app, tailwindcss, nextjs",
+  creator: "AnggaAdytya",
+  authors: {
+    name: "AnggaAdytya",
+    url: "https://github.com/AnggaAdytya",
+  },
+  openGraph: {
+    url: "https://my-weather-app.vercel.app",
+    siteName: "Weather App",
+    locale: "en",
+    type: "website",
+    images: [
+      {
+        url: "https://res.cloudinary.com/ddugt5n5v/image/upload/v1704298938/metadata/Screenshot_2024-01-03_232042_dkbem2.png",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://res.cloudinary.com/ddugt5n5v/image/upload/v1704298938/metadata/Screenshot_2024-01-03_232042_dkbem2.png",
+        width: 1200,
+        height: 630,
+      },
+      {
+        url: "https://res.cloudinary.com/ddugt5n5v/image/upload/v1704298938/metadata/Screenshot_2024-01-03_232042_dkbem2.png",
+        width: 1600,
+        height: 900,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
